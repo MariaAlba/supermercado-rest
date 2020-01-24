eadf9be59c1b4f68c547909ce6396555eaa2587e
Maria <Curso@PORT-Z12.ipartekaula.com>
2020-01-24 12:44:48 +0100

    order by params

--- src/main/java/com/ipartek/formacion/supermercado/controller/ProductoRestController.java
+++ src/main/java/com/ipartek/formacion/supermercado/controller/ProductoRestController.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Set;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -12,6 +13,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.apache.log4j.Logger;
 
@@ -20,6 +25,7 @@ import com.ipartek.formacion.supermercado.modelo.dao.ProductoDAO;
 import com.ipartek.formacion.supermercado.modelo.pojo.Producto;
 import com.ipartek.formacion.supermercado.pojo.ResponseMensaje;
 import com.ipartek.formacion.supermercado.utils.Utilidades;
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
 
 /**
  * Servlet implementation class ProductoRestController
@@ -35,21 +41,34 @@ public class ProductoRestController extends HttpServlet {
 
 	private String pathInfo;
 	private int statusCode;
-	private String jsonResponseBody;
+	private Object reponseBody;
+
+	private int idProducto;
+
+	private ValidatorFactory factory;
+	private Validator validator;
+
+	private Boolean listarOrdenado;
+	private String orden;
+	private String criterio;
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
 	public void init(ServletConfig config) throws ServletException {
-		// Inicializamos dao para poder hacer peticiones a bbdd
 		productoDao = ProductoDAO.getInstance();
+		factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
 	}
 
-	@Override
+	/**
+	 * @see Servlet#destroy()
+	 */
 	public void destroy() {
-		// Destruimos el dao
 		productoDao = null;
-
 	}
 
 	/**
@@ -59,183 +78,231 @@ public class ProductoRestController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		// RESPONSE: cabecera (la ponemos aquí porque se va a repetir en todos los
-		// métodos)
-
-		// Content type
-		response.setContentType("application/json");
-
-		// Charset
-		response.setCharacterEncoding("utf-8");
-
-		// cuerpo respuesta
-		jsonResponseBody = null;
-
-		pathInfo = request.getPathInfo();
-
-		LOG.debug("mirar pathInfo:" + pathInfo + " para saber si es listado o detalle");
-
-		// lama al resto e metodos doGet doPost doDelete doPut
-		super.service(request, response);
-
-		// TODO mostar parametros de url
-		LOG.debug("ahora entra en service despues de metodo que toke");
+		listarOrdenado = false;
 		LOG.debug(request.getMethod() + " " + request.getRequestURL());
 
-		response.setStatus(statusCode); // response status
+		orden = request.getParameter("_order");
+		criterio = request.getParameter("columna");
+		if (orden != null && criterio != null) {
+			listarOrdenado = true;
+		}
 
-		PrintWriter out = response.getWriter(); // out se encarga de escribir datos en el body
+//		Map<String, String[]> parametros = request.getParameterMap();
+//		for (String s : parametros.keySet()) {
+//			request.getParameter(s);
+//		}
+//		parametros.values();
+//		parametros.keySet();
 
-		out.print(jsonResponseBody.toString());
+		// prepara la response
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
 
-		out.flush(); // termina de escribir datos en body cierra el out
+		reponseBody = null;
+		pathInfo = request.getPathInfo();
+
+		try {
+
+			idProducto = Utilidades.obtenerId(pathInfo);
+
+			// llama a doGEt, doPost, doPut, doDelete
+			super.service(request, response);
+
+		} catch (Exception e) {
+
+			statusCode = HttpServletResponse.SC_BAD_REQUEST;
+			reponseBody = new ResponseMensaje(e.getMessage());
+
+		} finally {
+
+			response.setStatus(statusCode);
+
+			if (reponseBody != null) {
+				// response body
+				PrintWriter out = response.getWriter(); // out se encarga de poder escribir datos en el body
+				String jsonResponseBody = new Gson().toJson(reponseBody); // conversion de Java a Json
+				out.print(jsonResponseBody.toString());
+				out.flush();
+			}
+		}
+
 	}
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		LOG.trace("peticion GET");
-
-		if (pathInfo == null || "/".equals(pathInfo)) { // listar(request, response);
-
-			LOG.trace("entra en listar");
-
-			// Obtener productos de la BD
-			ArrayList<Producto> lista = (ArrayList<Producto>) productoDao.getAll();
-
-			// dejo la lista vacia si quiero que me ddevuelva 2014
-			// ArrayList<Producto> lista = new ArrayList<Producto>();
-
-			// hay que poner el status antes de escribir la response body
-			statusCode = (lista.isEmpty()) ? HttpServletResponse.SC_NO_CONTENT : HttpServletResponse.SC_OK;
-			;
-
-			// response
-			jsonResponseBody = new Gson().toJson(lista); // conversion de java a json
+		if (idProducto != -1) {
+			detalle(idProducto);
 		} else {
 
-			// detalle(request, response);
-			LOG.trace("Entra en detalle");
-
-			int id;
-			Producto producto = null;
-
-			try {
-				id = Utilidades.obtenerId(pathInfo);
-
-				if (id != -1) { // detalle
-					producto = productoDao.getById(id);
-				}
-
-				if (producto == null) {
-					statusCode = HttpServletResponse.SC_NOT_FOUND;
-					jsonResponseBody = new Gson().toJson(new ResponseMensaje("no se encuentra producto"));
-				} else {
-					statusCode = HttpServletResponse.SC_OK; // response.setStatus(200);//esto es lo mismo
-					jsonResponseBody = new Gson().toJson(producto); // conversion de java a json
-				}
-			} catch (Exception e) {
-				LOG.error(e);
-				statusCode = HttpServletResponse.SC_BAD_REQUEST;
-				jsonResponseBody = new Gson().toJson(new ResponseMensaje(e.getMessage()));
-			}
+			if (listarOrdenado == true)
+				listarporOrden(orden, criterio);
+			else
+				listar();
 		}
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		// convertir json del request body a Objeto
-		BufferedReader reader = request.getReader();
-		Gson gson = new Gson();
-		Producto producto = gson.fromJson(reader, Producto.class);
-
-		LOG.debug(" Json convertido a Objeto: " + producto);
-
-		// TODO validar Objeto
-
-		try {
-			productoDao.create(producto);
-			statusCode = HttpServletResponse.SC_CREATED;
-			jsonResponseBody = new Gson().toJson(producto);
-		} catch (Exception e) {
-			LOG.debug(e);
-			statusCode = HttpServletResponse.SC_CONFLICT;
-			jsonResponseBody = new Gson().toJson(new ResponseMensaje(e.getMessage()));
+	private void listarporOrden(String forma, String columna) {
+		ArrayList<Producto> productos = (ArrayList<Producto>) productoDao.getAllOrderBy(forma, columna);
+		reponseBody = productos;
+		if (productos.isEmpty()) {
+			statusCode = HttpServletResponse.SC_NO_CONTENT;
+		} else {
+			statusCode = HttpServletResponse.SC_OK;
 		}
-
 	}
 
-	protected void doPut(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	private void detalle(int id) {
 
-		LOG.debug("PUT editar recurso");
-
-		int id = 0;
-		Producto productoOriginal = null;
-
-		try {
-			id = Utilidades.obtenerId(pathInfo);
-			if (id != -1) {
-				productoOriginal = productoDao.getById(id);
-			}
-
-			if (productoOriginal == null) {
-				statusCode = HttpServletResponse.SC_NOT_FOUND;
-				jsonResponseBody = new Gson().toJson(new ResponseMensaje("Recurso no encontrado"));
-			} else {
-
-				// convertir json del request body a Objeto
-				BufferedReader reader = request.getReader();
-				Gson gson = new Gson();
-				Producto productoModificado = gson.fromJson(reader, Producto.class);
-
-				LOG.debug(" Json convertido a Objeto: " + productoModificado);
-
-				productoDao.update(id, productoModificado);
-				statusCode = HttpServletResponse.SC_OK;
-				jsonResponseBody = new Gson().toJson(productoModificado);
-
-			}
-
-		} catch (Exception e) {
-			LOG.debug(e);
-			statusCode = HttpServletResponse.SC_CONFLICT;
-			jsonResponseBody = new Gson().toJson(new ResponseMensaje(e.getMessage()));
+		reponseBody = productoDao.getById(id);
+		if (reponseBody != null) {
+			statusCode = HttpServletResponse.SC_OK;
+		} else {
+			reponseBody = null;
+			statusCode = HttpServletResponse.SC_NOT_FOUND;
 		}
-
 	}
 
+	private void listar() {
+
+		ArrayList<Producto> productos = (ArrayList<Producto>) productoDao.getAll();
+		reponseBody = productos;
+		if (productos.isEmpty()) {
+			statusCode = HttpServletResponse.SC_NO_CONTENT;
+		} else {
+			statusCode = HttpServletResponse.SC_OK;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		LOG.debug("DELETE recurso por id");
-
-		Producto producto = null;
-		int id = 0;
-
 		try {
-			id = Utilidades.obtenerId(pathInfo);
 
-			if (id != -1) { // detalle
-				producto = productoDao.getById(id);
-			}
+			Producto pEliminado = productoDao.delete(idProducto);
+			statusCode = HttpServletResponse.SC_OK;
+			reponseBody = pEliminado;
 
-			if (producto == null) {
-				statusCode = HttpServletResponse.SC_NOT_FOUND;
-				jsonResponseBody = new Gson().toJson(new ResponseMensaje("No se encuentra el recurso"));
-			} else {
-				productoDao.delete(id);
-				statusCode = HttpServletResponse.SC_OK;
-				jsonResponseBody = new Gson().toJson(new ResponseMensaje(producto + " eliminado"));
-			}
 		} catch (Exception e) {
-			LOG.error(e);
-			statusCode = HttpServletResponse.SC_BAD_REQUEST;
-			jsonResponseBody = new Gson().toJson(new ResponseMensaje(e.getMessage()));
+
+			statusCode = HttpServletResponse.SC_NOT_FOUND;
+			reponseBody = new ResponseMensaje(e.getMessage());
 		}
 
 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Producto pOut = null;
+
+		try {
+
+			// convertir json del request body a Objeto
+			BufferedReader reader = request.getReader();
+			Gson gson = new Gson();
+			Producto producto = gson.fromJson(reader, Producto.class);
+			LOG.debug(" Json convertido a Objeto: " + producto);
+
+			// validar objeto
+			Set<ConstraintViolation<Producto>> validacionesErrores = validator.validate(producto);
+			if (validacionesErrores.isEmpty()) {
+
+				if (producto.getId() == 0) {
+					pOut = productoDao.create(producto);
+					statusCode = HttpServletResponse.SC_CREATED;
+				} else {
+					pOut = productoDao.update(idProducto, producto);
+					statusCode = HttpServletResponse.SC_OK;
+				}
+				reponseBody = pOut;
+
+			} else {
+
+				statusCode = HttpServletResponse.SC_BAD_REQUEST;
+				ResponseMensaje responseMensaje = new ResponseMensaje("valores no correctos");
+				ArrayList<String> errores = new ArrayList<String>();
+				for (ConstraintViolation<Producto> error : validacionesErrores) {
+					errores.add(error.getPropertyPath() + " " + error.getMessage());
+				}
+				responseMensaje.setErrores(errores);
+				reponseBody = responseMensaje;
+
+			}
+
+		} catch (MySQLIntegrityConstraintViolationException e) {
+
+			statusCode = HttpServletResponse.SC_CONFLICT;
+			reponseBody = new ResponseMensaje("nombre de producto repetido");
+
+		} catch (Exception e) {
+
+			statusCode = HttpServletResponse.SC_BAD_REQUEST;
+			reponseBody = new ResponseMensaje(e.getMessage());
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		LOG.debug("PUT modificar recurso");
+		doPost(request, response);
+	}
+
+	// protected void doPut(HttpServletRequest request, HttpServletResponse
+	// response)
+//			throws ServletException, IOException {
+//
+//		LOG.debug("PUT editar recurso");
+//
+//		int id = 0;
+//		Producto productoOriginal = null;
+//
+//		try {
+//			id = Utilidades.obtenerId(pathInfo);
+//			if (id != -1) {
+//				productoOriginal = productoDao.getById(id);
+//			}
+//
+//			if (productoOriginal == null) {
+//				statusCode = HttpServletResponse.SC_NOT_FOUND;
+//				jsonResponseBody = new Gson().toJson(new ResponseMensaje("Recurso no encontrado"));
+//			} else {
+//
+//				// convertir json del request body a Objeto
+//				BufferedReader reader = request.getReader();
+//				Gson gson = new Gson();
+//				Producto productoModificado = gson.fromJson(reader, Producto.class);
+//
+//				LOG.debug(" Json convertido a Objeto: " + productoModificado);
+//
+//				productoDao.update(id, productoModificado);
+//				statusCode = HttpServletResponse.SC_OK;
+//				jsonResponseBody = new Gson().toJson(productoModificado);
+//
+//			}
+//
+//		} catch (Exception e) {
+//			LOG.debug(e);
+//			statusCode = HttpServletResponse.SC_CONFLICT;
+//			jsonResponseBody = new Gson().toJson(new ResponseMensaje(e.getMessage()));
+//		}
+//
+//	}
 
 }
